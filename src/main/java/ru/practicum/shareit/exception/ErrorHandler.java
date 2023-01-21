907655e7b935286ad7b20fd0563aa49a7c396cf2
Andrey Kretsev <akrecev@gmail.com>
2023-01-21 23:58:10 +0300

    fix: delete reliance on default encoding in ErrorHandler

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -10,10 +10,6 @@ import ru.practicum.shareit.exception.model.DataNotFoundException;
 import ru.practicum.shareit.exception.model.EmailConflictException;
 import ru.practicum.shareit.exception.model.MissingUserInRequestException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
@@ -22,50 +18,35 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleDataNotFoundException(final DataNotFoundException e) {
         log.error("404 {}", e.getMessage(), e);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
-
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleEmailConflictException(final EmailConflictException e) {
         log.error("409 {}", e.getMessage(), e);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
-
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         log.error("400 {}", e.getMessage(), e);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
-
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMissingUserInRequest(final MissingUserInRequestException e) {
         log.error("400 {}", e.getMessage(), e);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
-
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleInternalServerError(final Throwable e) {
         log.error("500 {}", e.getMessage(), e);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        e.printStackTrace(new PrintStream(out));
-
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(e.getMessage());
     }
 
 }
